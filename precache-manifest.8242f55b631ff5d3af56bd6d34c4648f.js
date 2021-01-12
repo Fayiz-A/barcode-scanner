3074ee6d710c9283a9019d189488fb05378c54e2
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-01-13 00:18:41 +0530

    Updates

---
+++ precache-manifest.8242f55b631ff5d3af56bd6d34c4648f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/barcode-scanner/expo-service-worker.js"
+  },
+  {
+    "revision": "667f95e23c41ace67324",
+    "url": "/barcode-scanner/static/js/app.19d10701.chunk.js"
+  },
+  {
+    "revision": "56d1f38c028f750a37a4",
+    "url": "/barcode-scanner/static/js/runtime~app.6692e88e.js"
+  },
+  {
+    "revision": "c0f3f94fd3b86a2cfd584d31439a49ac",
+    "url": "/barcode-scanner/static/media/barcodeScanner.871c3c0d.jpg"
+  },
+  {
+    "revision": "2896124908215aa3d76c22c183f985ea",
+    "url": "/barcode-scanner/static/js/2.31f736fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56367e04b365d1dae5b0",
+    "url": "/barcode-scanner/static/js/2.31f736fb.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/barcode-scanner/serve.json"
+  },
+  {
+    "revision": "7950222bdb05a67ea127e2068896075e",
+    "url": "/barcode-scanner/register-service-worker.js"
+  },
+  {
+    "revision": "01b7624e863a24f1e4121ce26150d435",
+    "url": "/barcode-scanner/manifest.json"
+  },
+  {
+    "revision": "8525acaae4b22d6f3944e173c3c30991",
+    "url": "/barcode-scanner/index.html"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/barcode-scanner/favicon.ico"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/barcode-scanner/favicon-32.png"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/barcode-scanner/favicon-16.png"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/barcode-scanner/expo-service-worker.js",{scope:"/barcode-scanner/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
